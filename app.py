aa7841821d2758bb5016fec78baf1ad2dc75bd03
enesituce <43863187+enesituce@users.noreply.github.com>
2021-01-17 01:14:30 +0300

    file organization and template changes

--- app.py
+++ app.py
@@ -9,6 +9,12 @@ app= Flask(__name__)
 def index():
     return render_template("index.html")
 
+
+@app.route("/channels")
+
+def channels():
+    return render_template("channels.html")
+
 if __name__=="__main__":
     app.run(debug = True)
 
